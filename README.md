0ab2905f74d7f072674744136b92ebd4be312c06
hajdam <hajdam@users.sf.net>
2019-07-21 22:09:20 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Default Popup Menu - IntelliJ Plugin
+====================================
+
+Registers default clipboard popup menu for regular components.
+
+Homepage: https://github.com/hajdam/defaultpopup-intellij-plugin  
+
+Published as: not yet
+
+Screenshot
+----------
+
+![Plugin Screenshot](images/screenshot.png?raw=true)
+
+Features
+--------
+
+ - Show popup menu with cut/copy/paste/delete and select all action
+ - Supported components JTextComponent, JList, JTable
+
+Compiling
+---------
+
+Java Development Kit (JDK) version 7 or later is required to build this project.
+
+IntelliJ Idea platform is necessary for build of this plugin. See. http://jetbrains.org  
+
+License
+-------
+
+Apache License, Version 2.0 - see LICENSE-2.0.txt
---
+++ src/org/exbin/utils/defaultpopup/DefaultPopupInstaller.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) ExBin Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.exbin.utils.defaultpopup;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Module installer.
+ *
+ * @version 0.1.0 2019/07/21
+ * @author ExBin Project (http://exbin.org)
+ */
+public class DefaultPopupInstaller implements StartupActivity {
+
+    private boolean installed = false;
+    @Override
+    public void runActivity(@NotNull Project project) {
+        if (!installed) {
+            ClipboardUtils.registerDefaultClipboardPopupMenu();
+            installed = true;
+        }
+    }
+
+//    @Override
+//    public void preload(@NotNull ProgressIndicator indicator) {
+//        WindowManager.getInstance().addListener(new WindowManagerListener() {
+//            @Override
+//            public void frameCreated(IdeFrame frame) {
+//                ClipboardUtils.registerDefaultClipboardPopupMenu();
+//            }
+//
+//            @Override
+//            public void beforeFrameReleased(IdeFrame frame) {
+//            }
+//        });
+//    }
+}
